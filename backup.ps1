7ed1ac3c067da586ea633e7135a3a992298c6cbd
Kushal-Chandar <kushalchandariit@gmail.com>
2022-06-16 17:46:31 +0530

    Winget and google drive done

--- backup.ps1
+++ backup.ps1
@@ -1,5 +1,7 @@
+# region Winget Packages Export
 winget export -o apps_temp.json | Out-Host
-$WingetPackagesExisting = @(((Get-Content .\scripts\applications\winget_install_apps.json -raw) -replace '(?m)(?<=^([^"]|"[^"]*")*)//.*' -replace '(?ms)/\*.*?\*/' | ConvertFrom-Json).PSObject.Properties | Select-Object Name, Value)
+$WingetPackagesExistingJson = (Get-Content .\scripts\applications\winget_install_apps.json -raw) -replace '(?m)(?<=^([^"]|"[^"]*")*)//.*' -replace '(?ms)/\*.*?\*/' | ConvertFrom-Json
+$WingetPackagesExisting = $WingetPackagesExistingJson.PSObject.Properties | ForEach-Object { "$($_.Name)" }
 $WingetPackagesNew = New-Object System.Collections.Generic.List[System.Object]
 $WingetSourceArray = (Get-Content .\apps_temp.json | ConvertFrom-Json).Sources
 for ($i = 0; $i -lt $WingetSourceArray.Count; $i++) {
@@ -8,5 +10,13 @@ for ($i = 0; $i -lt $WingetSourceArray.Count; $i++) {
         $WingetPackagesNew.Add($WingetPackagesArray[$j].PackageIdentifier)
     }
 }
-$WingetPackagesExisting
-$WingetPackagesNew
+$WingetPackagesAdd = @($WingetPackagesNew | Where-Object { $WingetPackagesExisting -notcontains "$_" })
+$WingetPackagesAdd | ForEach-Object { $WingetPackagesExistingJson | Add-Member -NotePropertyName $_ -NotePropertyValue 210 }
+"// | 0 = KVM | 1 = Coding and general gaming | 2 = Both | 210 = Undefined (need to set it manually) |`n" + ($WingetPackagesExistingJson | ConvertTo-Json) | Set-Content .\scripts\applications\winget_install_apps.json
+.\scripts\applications\winget_install_apps.json
+# endregion
+
+
+
+
+Start-Process "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Google Drive.lnk"
